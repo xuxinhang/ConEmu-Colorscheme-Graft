a01cda30fd49512e2b768feb3817730e24d95696
Xu Xinhang <xuxinhang4567#126.com>
2021-05-05 16:36:47 +0800

    Feat: Backup file and PowerShell compatibility.

--- graft.ps1
+++ graft.ps1
@@ -1,5 +1,6 @@
 
 $CONFIG_FILE_SEARCH_PATH = './ConEmu.xml'
+$CONFIG_FILE_BACKUP_PATH = './ConEmu.bak.xml'
 $COLORSCHEME_DIR_SEARCH_PATH = './ConEmu_Colorschemes'
 
 Function ParseColorschemeFile ($FileName, $FileContent) {
@@ -11,9 +12,9 @@ Function ParseColorschemeFile ($FileName, $FileContent) {
       'color8' , 'color9' , 'color10' , 'color11' , 'color12' , 'color13' , 'color14' , 'color15'
     )
 
-    $acceptable_keys | ForEach-Object {
+    $abc = $acceptable_keys | ForEach-Object {
       # TODO
-      if ([string]$FileContent -match "\.${_}:\s*#([a-z0-9A-Z]+?)\b") {
+      if ([string]$FileContent -match "${_}:\s*#([a-z0-9A-Z]+?)\b") {
         $ColorschemeObject[$_] = $matches[1]
       }
       else {
@@ -31,6 +32,10 @@ Function ParseColorschemeFile ($FileName, $FileContent) {
 }
 
 Function ConstructMarkupFragment ($DocRoot, $Colorscheme) {
+  Function _CC ($c) {
+    $c.SubString(4, 2) + $c.SubString(2, 2) + $c.SubString(0, 2)
+  }
+
   $refDoc = [xml]@"
     <key name="Palette?">
       <value name="Name" type="string" data="CCSG__$($Colorscheme.Name)"/>
@@ -38,22 +43,22 @@ Function ConstructMarkupFragment ($DocRoot, $Colorscheme) {
       <value name="BackColorIdx" type="hex" data="10"/>
       <value name="PopTextColorIdx" type="hex" data="10"/>
       <value name="PopBackColorIdx" type="hex" data="10"/>
-      <value name="ColorTable00" type="dword" data="00$($Colorscheme.color0 )"/>
-      <value name="ColorTable01" type="dword" data="00$($Colorscheme.color1 )"/>
-      <value name="ColorTable02" type="dword" data="00$($Colorscheme.color2 )"/>
-      <value name="ColorTable03" type="dword" data="00$($Colorscheme.color3 )"/>
-      <value name="ColorTable04" type="dword" data="00$($Colorscheme.color4 )"/>
-      <value name="ColorTable05" type="dword" data="00$($Colorscheme.color5 )"/>
-      <value name="ColorTable06" type="dword" data="00$($Colorscheme.color6 )"/>
-      <value name="ColorTable07" type="dword" data="00$($Colorscheme.color7 )"/>
-      <value name="ColorTable08" type="dword" data="00$($Colorscheme.color8 )"/>
-      <value name="ColorTable09" type="dword" data="00$($Colorscheme.color9 )"/>
-      <value name="ColorTable10" type="dword" data="00$($Colorscheme.color10)"/>
-      <value name="ColorTable11" type="dword" data="00$($Colorscheme.color11)"/>
-      <value name="ColorTable12" type="dword" data="00$($Colorscheme.color12)"/>
-      <value name="ColorTable13" type="dword" data="00$($Colorscheme.color13)"/>
-      <value name="ColorTable14" type="dword" data="00$($Colorscheme.color14)"/>
-      <value name="ColorTable15" type="dword" data="00$($Colorscheme.color15)"/>
+      <value name="ColorTable00" type="dword" data="00$(_CC($Colorscheme.color0 ))"/>
+      <value name="ColorTable01" type="dword" data="00$(_CC($Colorscheme.color4 ))"/>
+      <value name="ColorTable02" type="dword" data="00$(_CC($Colorscheme.color2 ))"/>
+      <value name="ColorTable03" type="dword" data="00$(_CC($Colorscheme.color6 ))"/>
+      <value name="ColorTable04" type="dword" data="00$(_CC($Colorscheme.color1 ))"/>
+      <value name="ColorTable05" type="dword" data="00$(_CC($Colorscheme.color5 ))"/>
+      <value name="ColorTable06" type="dword" data="00$(_CC($Colorscheme.color3 ))"/>
+      <value name="ColorTable07" type="dword" data="00$(_CC($Colorscheme.color7 ))"/>
+      <value name="ColorTable08" type="dword" data="00$(_CC($Colorscheme.color8 ))"/>
+      <value name="ColorTable09" type="dword" data="00$(_CC($Colorscheme.color12))"/>
+      <value name="ColorTable10" type="dword" data="00$(_CC($Colorscheme.color10))"/>
+      <value name="ColorTable11" type="dword" data="00$(_CC($Colorscheme.color14))"/>
+      <value name="ColorTable12" type="dword" data="00$(_CC($Colorscheme.color9 ))"/>
+      <value name="ColorTable13" type="dword" data="00$(_CC($Colorscheme.color13))"/>
+      <value name="ColorTable14" type="dword" data="00$(_CC($Colorscheme.color11))"/>
+      <value name="ColorTable15" type="dword" data="00$(_CC($Colorscheme.color15))"/>
     </key>
 "@
 
@@ -74,6 +79,7 @@ Function MainEntry () {
   # read and parse colorschemes
   $AttachedColorschemes = Get-ChildItem $COLORSCHEME_DIR_SEARCH_PATH |
   ForEach-Object { ParseColorschemeFile $_.Name (Get-Content $_.FullName) }
+  Write-Output $AttachedColorschemes.Count
 
   # append updated nodes
   $AttachedColorschemes | ForEach-Object {
@@ -83,13 +89,17 @@ Function MainEntry () {
 
   # rewrite counts
   $ColorschemeCount = $ConfigColorRoot.ChildNodes.Count - 1
-  $ConfigColorCountNode.data = $ColorschemeCount
+  $ConfigColorCountNode.data = [string]$ColorschemeCount
   For ($i = 0; $i -lt $ColorschemeCount; $i++) {
     $ConfigColorRoot.key[$i].name = "Palette$($i + 1)"
   }
 
   # write to file
-  $ConfigDocumentRoot.Save('./New_Config.xml')
+  If (Get-Item $CONFIG_FILE_SEARCH_PATH) {
+    Move-Item $CONFIG_FILE_SEARCH_PATH $CONFIG_FILE_BACKUP_PATH -Force
+  }
+  $ConfigDocumentRoot.Save($CONFIG_FILE_SEARCH_PATH)
+  # $ConfigDocumentRoot.Save('New_Conf.xml')
 }
 
 
